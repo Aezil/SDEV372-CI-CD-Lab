6ebe20ba4e7f42e92f9b682d038246640dbb0d23
Lee <trueleion@gmail.com>
2023-03-17 17:36:57 -0700

    chore: added tests to CdLabsApplicationTest file

--- src/test/java/edu/greenriver/sdev/cdlab/CdLabApplicationTests.java
+++ src/test/java/edu/greenriver/sdev/cdlab/CdLabApplicationTests.java
@@ -1,13 +1,52 @@
 package edu.greenriver.sdev.cdlab;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CdLabApplicationTests {
 
+    private final int SIDES = 6;
+    private final String COLOR = "White";
+    private Dice dice;
+
+    @BeforeEach()
+    void init() {
+        dice = new Dice(SIDES, COLOR);
+    }
+
     @Test
-    void contextLoads() {
+    void testSides() {
+        assertEquals(dice.getSides(), SIDES);
+    }
+
+    @Test
+    void testColor() {
+        assertEquals(dice.getColor(), COLOR);
+    }
+
+    @Test
+    void testRollDice() {
+        int roll = -1;
+        while (roll != 3) {
+            roll = dice.roll();
+        }
+        assertEquals(roll, 3);
+    }
+
+    @Test
+    void testRollMany() {
+        int rollAmount = 10;
+        int[] history = dice.rollMany(rollAmount);
+        assertEquals(rollAmount, history.length);
+    }
+
+    @Test
+    void testToString() {
+        String actual = "A " + SIDES + " sided die";
+        assertEquals(dice.toString(), actual);
     }
 
 }
