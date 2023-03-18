05a45cb930727d9f76622efa0c225f41ef7d8eb7
Lee <trueleion@gmail.com>
2023-03-17 17:30:56 -0700

    init commit

---
+++ src/test/java/edu/greenriver/sdev/cdlab/CdLabApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.cdlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
